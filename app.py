91e58ea8479ff08414e4543ed26025488c261c98
Shani Levi <sshanile50@gmail.com>
2019-07-23 21:22:36 +0300

    add scheduler and deactivate

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restplus import Resource, Api
 from enum import Enum
+from apscheduler.schedulers.background import BackgroundScheduler
 import models
 import random
 
@@ -17,14 +18,28 @@ parser = api.parser()
 parser.add_argument('model', type=Model, choices=list(Model))
 parser.add_argument('seconds', type=int)
 
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+
+def call_models_job(model):
+    numbers = [random.random() for _ in range(10)]
+    return models.mean(numbers) if model == Model.MEAN else models.median(numbers)
+
 
 @api.route('/activate')
-class InvokeModels(Resource):
+class ActivateModels(Resource):
     def post(self):
         args = parser.parse_args()
         model = args['model']
-        numbers = [random.random() for _ in range(10)]
-        return models.mean(numbers) if model == Model.MEAN else models.median(numbers)
+        sec = args['seconds']
+        scheduler.add_job(call_models_job, 'interval', seconds=sec, args=[model])
+
+
+@api.route('/deactivate')
+class DeactivateModels(Resource):
+    def post(self):
+        scheduler.remove_all_jobs()
 
 
 if __name__ == '__main__':
