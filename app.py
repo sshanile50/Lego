af41b5abc2a1c0ebc964228dbc2f18687de71ac6
Shani Levi <sshanile50@gmail.com>
2019-07-24 17:44:07 +0300

    use flask-sqlalchemy to store the results

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_restplus import Resource, Api
 from enum import Enum
 from apscheduler.schedulers.background import BackgroundScheduler
+from flask_sqlalchemy import SQLAlchemy
 import models
 import random
+import datetime
 
 
 class Model(Enum):
@@ -12,7 +14,19 @@ class Model(Enum):
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:75210@localhost:5432/modelsResults'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 api = Api(app)
+
+
+class Result(db.Model):
+    result = db.Column(db.Float)
+    model_name = db.Column(db.PickleType)
+    time = db.Column(db.DateTime, primary_key=True)
+
+
+db.create_all()
 
 parser = api.parser()
 parser.add_argument('model', type=Model, choices=list(Model))
@@ -24,7 +38,9 @@ scheduler.start()
 
 def call_models_job(model):
     numbers = [random.random() for _ in range(10)]
-    return models.mean(numbers) if model == Model.MEAN else models.median(numbers)
+    res = models.mean(numbers) if model == Model.MEAN else models.median(numbers)
+    db.session.add(Result(result=res, model_name=model, time=datetime.datetime.now()))
+    db.session.commit()
 
 
 @api.route('/activate')
@@ -40,6 +56,7 @@ class ActivateModels(Resource):
 class DeactivateModels(Resource):
     def post(self):
         scheduler.remove_all_jobs()
+        db.drop_all()
 
 
 if __name__ == '__main__':
